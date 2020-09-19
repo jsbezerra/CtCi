7a9c365f62cfe7ab36192c320cc062b76b64c1a9
Jonas Bezerra <jonassantosbezerra@outlook.com>
2020-09-18 23:48:21 -0300

    feat(graphs): adds dfs sketch

--- lib/ADT/graph.rb
+++ lib/ADT/graph.rb
@@ -9,15 +9,28 @@ module ADT
     def add_node(node)
       @vertices << node
     end
+
+    def dfs(root)
+      return if root.nil?
+      root.visited = true
+      if block_given?
+        yield(root)
+      end
+      root.adjacent.each do |node|
+        dfs(node) unless node.visited
+      end
+    end
   end
 
   class Node
     attr_reader :adjacent
     attr_reader :vertex
+    attr_accessor :visited
 
     def initialize(vertex)
       @vertex = vertex
       @adjacent = []
+      @visited = false
     end
 
     def add_adjacent(node)
