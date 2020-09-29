c4a370eff8dd6dc9ff15f5be80f697e5952c944a
Jonas Bezerra <jonassantosbezerra@outlook.com>
2020-09-28 21:06:52 -0300

    test(1.6): string compression

--- lib/chapter_1/strings.rb
+++ lib/chapter_1/strings.rb
@@ -136,5 +136,13 @@ module Chapter1
       end
       return !found_difference || (i == chars1.size && j == chars2.size)
     end
+
+    # Q1.6) String Compression: Implement a method to perform basic string compression using the counts of repeated
+    # characters. For example, the string "aabcccccaaa" would become "a2b1c5a3". If the compressed string would not
+    # become smaller  than the original string, your method should return the original string. You can assume the string
+    # has only uppercase and lowercase letters (a - z).
+    def self.string_compression(str)
+
+    end
   end
 end
---
+++ spec/chapter_1/q1_6_string_compression_spec.rb
@@ -0,0 +1,75 @@
+require 'rspec'
+require 'chapter_1/strings'
+
+describe 'Strings#string_compression' do
+
+  context 'when given "aabcccccaaa"' do
+    it 'returns "a2b1c5a3"' do
+      pending
+      expect(Chapter1::Strings.string_compression('aabcccccaaa')).to eq('a2b1c5a3')
+    end
+  end
+
+  context 'when given "aabbccddeeffgghh"' do
+    it 'returns "aabbccddeeffgghh"' do
+      pending
+      expect(Chapter1::Strings.string_compression('aabbccddeeffgghh')).to eq('aabbccddeeffgghh')
+    end
+  end
+
+  context 'when given "aabbccddeeeffgghh"' do
+    it 'returns "a2b2c2d2e3f2g2h2"' do
+      pending
+      expect(Chapter1::Strings.string_compression('aabbccddeeeffgghh')).to eq('a2b2c2d2e3f2g2h2')
+    end
+  end
+
+  context 'when given "abcdefghijkl"' do
+    it 'returns "abcdefghijkl"' do
+      pending
+      expect(Chapter1::Strings.string_compression('abcdefghijkl')).to eq('abcdefghijkl')
+    end
+  end
+
+  context 'when given "aaaaaaaaaa"' do
+    it 'returns "a10"' do
+      pending
+      expect(Chapter1::Strings.string_compression('aaaaaaaaaa')).to eq('a10')
+    end
+  end
+
+  context 'when given "aaaaaaaaaabcb"' do
+    it 'returns "a10b1c1b1"' do
+      pending
+      expect(Chapter1::Strings.string_compression('aaaaaaaaaabcb')).to eq('a10b1c1b1')
+    end
+  end
+
+  context 'when given "aaaaaaaaaabcb"' do
+    it 'returns "a10b1c1b1"' do
+      pending
+      expect(Chapter1::Strings.string_compression('aaaaaaaaaabcb')).to eq('a10b1c1b1')
+    end
+  end
+
+  context 'when given "aaaaaaaaaabcbabc"' do
+    it 'returns "a10b1c1b1a1b1c1"' do
+      pending
+      expect(Chapter1::Strings.string_compression('aaaaaaaaaabcbabc')).to eq('a10b1c1b1a1b1c1')
+    end
+  end
+
+  context 'when given "aaaaaaaaaabcbabca"' do
+    it 'returns "aaaaaaaaaabcbabca"' do
+      pending
+      expect(Chapter1::Strings.string_compression('aaaaaaaaaabcbabca')).to eq('aaaaaaaaaabcbabca')
+    end
+  end
+
+  context 'when given "aaaaaaaaaabcbabcabc"' do
+    it 'returns "aaaaaaaaaabcbabcabc"' do
+      pending
+      expect(Chapter1::Strings.string_compression('aaaaaaaaaabcbabcabc')).to eq('aaaaaaaaaabcbabcabc')
+    end
+  end
+end
