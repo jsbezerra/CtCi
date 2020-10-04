f7318a80c78171b122d90501582500c653d7bd0b
Jonas Bezerra <jonassantosbezerra@outlook.com>
2020-10-04 17:10:15 -0300

    question(3.2): stack min

---
+++ lib/chapter3/stacks.rb
@@ -0,0 +1,35 @@
+require 'adt/stack'
+
+module ADT
+
+  # Q3.2) Stack Min: How would you design a stack which, in addition to +push+ and +pop+, has a function +min+ which
+  # returns the minimum element? +push+, +pop+ and +min+ should all operate in O(1) time.
+  class StackMin < ADT::Stack
+
+    def push(data)
+      if empty?
+        element = [data, data]
+      else
+        min = [data, self.min].min
+        element = [data, min]
+      end
+      super(element)
+    end
+
+    def pop
+      return nil if empty?
+      element = super
+      element[0]
+    end
+
+    def peek
+      return nil if empty?
+      @top.data[0]
+    end
+
+    def min
+      return nil if empty?
+      @top.data[1]
+    end
+  end
+end
