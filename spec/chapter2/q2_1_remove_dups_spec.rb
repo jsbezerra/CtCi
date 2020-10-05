cb0a631ffd4a1ac8874b4e1d53921fac96b5a512
Jonas Bezerra <jonassantosbezerra@outlook.com>
2020-10-04 21:13:53 -0300

    test: increase code coverage

--- spec/chapter2/q2_1_remove_dups_spec.rb
+++ spec/chapter2/q2_1_remove_dups_spec.rb
@@ -3,106 +3,108 @@ require 'chapter2/linked_lists'
 
 describe 'SLList#remove_dups!' do
 
-  context 'when given a list {1,1,1,1,1}' do
-    it 'returns only {1}' do
-      list = ADT::List::SLList.new
-      5.times {
-        list.add(1)
-      }
-      expect(list.size).to eq(5)
-      expect(list.to_a).to eq([1, 1, 1, 1, 1])
+  example '{1,1,1,1,1} should return {1}' do
+    list = ADT::List::SLList.new
+    5.times {
+      list.add(1)
+    }
+    expect(list.size).to eq(5)
+    expect(list.to_a).to eq([1, 1, 1, 1, 1])
 
-      list.remove_dups!
-      expect(list.size).to eq(1)
-      expect(list.to_a).to eq([1])
-    end
+    list.remove_dups!
+    expect(list.size).to eq(1)
+    expect(list.to_a).to eq([1])
   end
 
-  context 'when given a list with several 1s, 2s and 3s' do
-    it 'returns only {1,2,3}' do
-      list = ADT::List::SLList.new
-      arr = []
-      5.times {
-        list.add(1)
-        list.add(2)
-        list.add(3)
-        arr.append(1, 2, 3)
-      }
-      expect(list.size).to eq(15)
-      expect(list.to_a).to eq(arr)
+  example 'a list with several 1s, 2s and 3s should return only {1,2,3}' do
+    list = ADT::List::SLList.new
+    arr = []
+    5.times {
+      list.add(1)
+      list.add(2)
+      list.add(3)
+      arr.append(1, 2, 3)
+    }
+    expect(list.size).to eq(15)
+    expect(list.to_a).to eq(arr)
 
-      list.remove_dups!
-      expect(list.size).to eq(3)
-      expect(list.to_a).to eq([1, 2, 3])
-    end
+    list.remove_dups!
+    expect(list.size).to eq(3)
+    expect(list.to_a).to eq([1, 2, 3])
   end
 
-  context 'when given a list without repeated elements' do
-    it 'returns the same list' do
-      list = ADT::List::SLList.new
-      arr = (1..20).to_a
-      (1..20).each { |i|
-        list.add(i)
-      }
-      expect(list.size).to eq(20)
-      expect(list.to_a).to eq(arr)
+  example 'list without repeated elements should be left unaltered' do
+    list = ADT::List::SLList.new
+    arr = (1..20).to_a
+    (1..20).each { |i|
+      list.add(i)
+    }
+    expect(list.size).to eq(20)
+    expect(list.to_a).to eq(arr)
 
-      list.remove_dups!
-      expect(list.size).to eq(20)
-      expect(list.to_a).to eq(arr)
-    end
+    list.remove_dups!
+    expect(list.size).to eq(20)
+    expect(list.to_a).to eq(arr)
+  end
+
+  example "an empty list should remain empty" do
+    list = ADT::List::SLList.new
+    list.remove_dups!
+    expect(list.empty?).to be_truthy
+    expect(list.size).to eq(0)
   end
 end
 
 describe 'SLList#remove_dups_b!' do
 
-  context 'when given a list {1,1,1,1,1}' do
-    it 'returns only {1}' do
-      list = ADT::List::SLList.new
-      5.times {
-        list.add(1)
-      }
-      expect(list.size).to eq(5)
-      expect(list.to_a).to eq([1, 1, 1, 1, 1])
+  example '{1,1,1,1,1} should return {1}' do
+    list = ADT::List::SLList.new
+    5.times {
+      list.add(1)
+    }
+    expect(list.size).to eq(5)
+    expect(list.to_a).to eq([1, 1, 1, 1, 1])
 
-      list.remove_dups_b!
-      expect(list.size).to eq(1)
-      expect(list.to_a).to eq([1])
-    end
+    list.remove_dups_b!
+    expect(list.size).to eq(1)
+    expect(list.to_a).to eq([1])
   end
 
-  context 'when given a list with several 1s, 2s and 3s' do
-    it 'returns only {1,2,3}' do
-      list = ADT::List::SLList.new
-      arr = []
-      5.times {
-        list.add(1)
-        list.add(2)
-        list.add(3)
-        arr.append(1, 2, 3)
-      }
-      expect(list.size).to eq(15)
-      expect(list.to_a).to eq(arr)
+  example 'a list with several 1s, 2s and 3s should return {1,2,3}' do
+    list = ADT::List::SLList.new
+    arr = []
+    5.times {
+      list.add(1)
+      list.add(2)
+      list.add(3)
+      arr.append(1, 2, 3)
+    }
+    expect(list.size).to eq(15)
+    expect(list.to_a).to eq(arr)
 
-      list.remove_dups_b!
-      expect(list.size).to eq(3)
-      expect(list.to_a).to eq([1, 2, 3])
-    end
+    list.remove_dups_b!
+    expect(list.size).to eq(3)
+    expect(list.to_a).to eq([1, 2, 3])
   end
 
-  context 'when given a list without repeated elements' do
-    it 'returns the same list' do
-      list = ADT::List::SLList.new
-      arr = (1..20).to_a
-      (1..20).each { |i|
-        list.add(i)
-      }
-      expect(list.size).to eq(20)
-      expect(list.to_a).to eq(arr)
+  example 'a list without repeated elements should be left unaltered' do
+    list = ADT::List::SLList.new
+    arr = (1..20).to_a
+    (1..20).each { |i|
+      list.add(i)
+    }
+    expect(list.size).to eq(20)
+    expect(list.to_a).to eq(arr)
 
-      list.remove_dups_b!
-      expect(list.size).to eq(20)
-      expect(list.to_a).to eq(arr)
-    end
+    list.remove_dups_b!
+    expect(list.size).to eq(20)
+    expect(list.to_a).to eq(arr)
+  end
+
+  example "an empty list should remain empty" do
+    list = ADT::List::SLList.new
+    list.remove_dups_b!
+    expect(list.empty?).to be_truthy
+    expect(list.size).to eq(0)
   end
 end
