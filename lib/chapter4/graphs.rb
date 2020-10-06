35efb458b95532ee5ac6cb754162e485970d259e
Jonas Bezerra <jonassantosbezerra@outlook.com>
2020-10-06 15:12:37 -0300

    test(4.7): build order

--- lib/chapter4/graphs.rb
+++ lib/chapter4/graphs.rb
@@ -32,6 +32,14 @@ module ADT
       aux_route_dfs?(source_key, target_key, visited)
     end
 
+    # Q4.7) You are given a list of products and a list of dependencies  (which is a list of pairs of projects, where
+    # the second project is dependent on the first project). All of a project's dependencies must be built before the
+    # project is. Find a build order that will allow the projects to be built. If there is no valid build order, return
+    # an empty array.
+    def build_order
+      throw NotImplementedError
+    end
+
     private
 
     def aux_route_dfs?(source_key, target_key, visited)
