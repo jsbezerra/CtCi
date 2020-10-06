604f384b843a85956bcfe77885c8ba393ae562db
Jonas Bezerra <jonassantosbezerra@outlook.com>
2020-10-06 11:08:11 -0300

    refactor: move balanced_tree_10_nodes to helper module

--- spec/adt/binary_tree_spec.rb
+++ spec/adt/binary_tree_spec.rb
@@ -1,23 +1,10 @@
 require 'rspec'
 require 'adt/binary_tree'
+require 'support/binary_tree_helper'
 
 describe 'TreeNode' do
 
-  def balanced_tree_10_nodes
-    root = ADT::BinaryTree::Node.new(5)
-
-    root.insert_left(ADT::BinaryTree::Node.new(3))
-    root.left.insert_left(ADT::BinaryTree::Node.new(1))
-    root.left.left.insert_right(ADT::BinaryTree::Node.new(2))
-    root.left.insert_right(ADT::BinaryTree::Node.new(4))
-
-    root.insert_right(ADT::BinaryTree::Node.new(8))
-    root.right.insert_left(ADT::BinaryTree::Node.new(6))
-    root.right.left.insert_right(ADT::BinaryTree::Node.new(7))
-    root.right.insert_right(ADT::BinaryTree::Node.new(10))
-    root.right.right.insert_left(ADT::BinaryTree::Node.new(9))
-    root
-  end
+  include BinaryTreeHelper
 
   context '#new' do
     it 'creates a new tree node' do
---
+++ spec/support/binary_tree_helper.rb
@@ -0,0 +1,17 @@
+module BinaryTreeHelper
+  def balanced_tree_10_nodes
+    root = ADT::BinaryTree::Node.new(5)
+
+    root.insert_left(ADT::BinaryTree::Node.new(3))
+    root.left.insert_left(ADT::BinaryTree::Node.new(1))
+    root.left.left.insert_right(ADT::BinaryTree::Node.new(2))
+    root.left.insert_right(ADT::BinaryTree::Node.new(4))
+
+    root.insert_right(ADT::BinaryTree::Node.new(8))
+    root.right.insert_left(ADT::BinaryTree::Node.new(6))
+    root.right.left.insert_right(ADT::BinaryTree::Node.new(7))
+    root.right.insert_right(ADT::BinaryTree::Node.new(10))
+    root.right.right.insert_left(ADT::BinaryTree::Node.new(9))
+    root
+  end
+end
