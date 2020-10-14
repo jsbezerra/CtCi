65178cee5caad6d1321dec96e10d2112088f9a49
Jonas Bezerra <jonassantosbezerra@outlook.com>
2020-10-14 15:53:56 -0300

    test(4.10): check subtree - in place

--- spec/chapter4/q4_10_check_subtree_spec.rb
+++ spec/chapter4/q4_10_check_subtree_spec.rb
@@ -75,3 +75,87 @@ describe 'Node#check_subtree' do
     end
   end
 end
+
+describe 'Node#check_subtree_in_place' do
+
+  include BinaryTreeHelper
+
+  before(:context) do
+    @root = balanced_bst_10_nodes
+    @nodes = @root.in_order_nodes
+  end
+
+  context 'subtrees with nodes directly from the bigger tree' do
+    example "1(nil,2) should be a subtree" do
+      pending 'Not implemented yet'
+      expect(@root.check_subtree_in_place(@nodes[0])).to eq(true)
+    end
+
+    example "8(6(nil,7),10(9,nil)) should be a subtree" do
+      pending 'Not implemented yet'
+      expect(@root.check_subtree_in_place(@nodes[7])).to eq(true)
+    end
+
+    example "7 should be a subtree" do
+      pending 'Not implemented yet'
+      expect(@root.check_subtree_in_place(@nodes[6])).to eq(true)
+    end
+  end
+
+  context 'subtrees created separately from the bigger tree' do
+    example "1(nil,2) should be a subtree" do
+      pending 'Not implemented yet'
+      root2 = ADT::BinaryTree::Node.new 1
+      root2.insert_right(ADT::BinaryTree::Node.new 2)
+      expect(@root.check_subtree_in_place(root2)).to eq(true)
+    end
+
+    example "8(6(nil,7),10(9,nil)) should be a subtree" do
+      pending 'Not implemented yet'
+      root2 = ADT::BinaryTree::Node.new 8
+      root2.insert_left(ADT::BinaryTree::Node.new 6)
+      root2.insert_right(ADT::BinaryTree::Node.new 10)
+      root2.left.insert_right(ADT::BinaryTree::Node.new 7)
+      root2.right.insert_left(ADT::BinaryTree::Node.new 9)
+      expect(@root.check_subtree_in_place(root2)).to eq(true)
+    end
+
+    example "7 should be a subtree" do
+      pending 'Not implemented yet'
+      root2 = ADT::BinaryTree::Node.new 7
+      expect(@root.check_subtree_in_place(root2)).to eq(true)
+    end
+
+    example "empty tree is always a subtree" do
+      pending 'Not implemented yet'
+      expect(@root.check_subtree_in_place(nil)).to eq(true)
+    end
+  end
+
+  context 'not subtrees' do
+    example "1(0,2) should be a subtree" do
+      pending 'Not implemented yet'
+      root2 = ADT::BinaryTree::Node.new 1
+      root2.insert_left(ADT::BinaryTree::Node.new 0)
+      root2.insert_right(ADT::BinaryTree::Node.new 2)
+      expect(@root.check_subtree_in_place(root2)).to eq(false)
+    end
+
+    example "8(6(nil,7),10(9,11)) should be a subtree" do
+      pending 'Not implemented yet'
+      root2 = ADT::BinaryTree::Node.new 8
+      root2.insert_left(ADT::BinaryTree::Node.new 6)
+      root2.insert_right(ADT::BinaryTree::Node.new 10)
+      root2.left.insert_right(ADT::BinaryTree::Node.new 7)
+      root2.right.insert_left(ADT::BinaryTree::Node.new 9)
+      root2.right.insert_right(ADT::BinaryTree::Node.new 11)
+      expect(@root.check_subtree_in_place(root2)).to eq(false)
+    end
+
+    example "11 should not be a subtree" do
+      pending 'Not implemented yet'
+      root2 = ADT::BinaryTree::Node.new 11
+      expect(@root.check_subtree_in_place(root2)).to eq(false)
+    end
+  end
+end
